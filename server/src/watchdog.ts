c75b777327c7d423fc43e0db0aa87d8ed5f4faff
degory <degory@users.noreply.github.com>
2024-03-13 19:24:25 +0100

    Bug fixes and stability improvements (#57)
    Enhancements:
    - Adjust the watchdog timeout automatically based on timings reported by the compiler
    - Highlight the `field` keyword as a storage class
    
    Bugs fixed:
    - Hover and references don't work reliably for cursor positions immediately adjacent to symbols

--- server/src/watchdog.ts
+++ server/src/watchdog.ts
@@ -1,11 +1,49 @@
 
 import { rejectAllAndThrow } from './extension-state';
 
+/*
+type Timer = NodeJS.Timeout & { getRemainingTime: () => number };
+
+function createTimer(callback: () => void, delay: number): Timer {
+    const startTime = Date.now();
+    const timer = setTimeout(callback, delay) as Timer;
+
+    timer.getRemainingTime = () => {
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - startTime;
+        return Math.max(delay - elapsedTime, 0);
+    };
+
+    return timer;
+}
+*/
+
+type Timer = NodeJS.Timer;
+
+function createTimer(callback: () => void, delay: number): Timer {
+    return setTimeout(callback, delay);
+}
+
 export class Watchdog {
-    watchdog_timer: NodeJS.Timer;
+    watchdog_timer: Timer;
+    timeout_milliseconds: number;
+
+    constructor(timeout_milliseconds: number) {
+        this.timeout_milliseconds = timeout_milliseconds;
+    }
+
+    setTimeout(timeout_milliseconds: number) {
+        this.timeout_milliseconds = timeout_milliseconds > 1000 ? timeout_milliseconds : 1000;
+    }
+
+    startWatchdogIfNotRunning() {
+        if (!this.watchdog_timer) {
+            this.startWatchdog();
+        }
+    }
 
     startWatchdog() {
-        this.watchdog_timer = setTimeout(() => { this.onWatchdogTimeout() }, 10000);
+        this.watchdog_timer = createTimer(() => { this.onWatchdogTimeout() }, this.timeout_milliseconds);
     }
 
     resetWatchdog() {
@@ -16,6 +54,7 @@ export class Watchdog {
     clearWatchdog() {
         if (this.watchdog_timer) {
             clearTimeout(this.watchdog_timer);
+            this.watchdog_timer = null;
         }
     }
     
